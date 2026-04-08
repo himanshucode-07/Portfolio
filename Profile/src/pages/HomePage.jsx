b1890ecb551aa90bf20561a9a9c4f27c2f49a58d
himanshucode-07 <himanshusinghthakur23@gmail.com>
2026-04-08 08:14:49 +0530

    setup react router with basic pages and navigation

---
+++ Profile/src/pages/HomePage.jsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <h1>Himanshu Home Page</h1>
+}
---
+++ Profile/src/pages/ProjectPage.jsx
@@ -0,0 +1,3 @@
+export default function ProjectPage() {
+  return <h1 className="text-">Projects Page</h1>
+}
